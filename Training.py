da7f9e75721c7c55dfb583ea2c761b6095f73aac
liliana <48772602+lilaorlova@users.noreply.github.com>
2023-10-26 10:23:26 +0300

    Update Training.py

--- Training.py
+++ Training.py
@@ -2,6 +2,10 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from Model import Net
+import torch
+from Dataloader import trainloader, batch_size, classes
+
 
 net = Net()
 
