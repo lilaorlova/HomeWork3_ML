b0d66a8e45bc39508e5ea43a34eea6837c098f61
liliana <48772602+lilaorlova@users.noreply.github.com>
2023-11-05 23:53:12 +0300

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,37 +1,30 @@
-import torch.optim as optim
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from Model import Net
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, MNIST
+import os
+
+from lightning.pytorch.loggers import WandbLogger
+import lightning as L
 import torch
-from Dataloader import trainloader, batch_size, classes
+import torchvision
+import torch.nn.functional as F
+from torch import nn
+from torch.utils.data import DataLoader, random_split
+from torchmetrics.functional import accuracy
+from torchvision import transforms
+from CIFAR10DataModule import CIFAR10DataModule
+from CIFAR10Model import CIFAR10Model
 
+dm = CIFAR10DataModule()
 
-net = Net()
+wandb_logger = WandbLogger(project='wandb-cifar10', job_type='train')
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-for epoch in range(2):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-
-print('Finished Training')
+model = CIFAR10Model()
+trainer = L.Trainer(
+    max_epochs=1,
+    accelerator="auto",
+    devices=1,
+    logger=wandb_logger,
+)
+trainer.fit(model, dm)
+trainer.test()
+wandb.finish()
